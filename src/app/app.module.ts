6ebed32b2b3df2df8b85e0a46b0a2964b53a7376
Ed <picassofb@hotmail.com>
2019-09-03 16:07:19 -0500

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientEditComponent } from './components/client-edit/client-edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientEditComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigurationComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/client-edit/client-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-edit',
+  templateUrl: './client-edit.component.html',
+  styles: []
+})
+export class ClientEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
